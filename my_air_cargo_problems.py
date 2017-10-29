06211a7a62bced5dbc84a495d87d41cf06cb8337
AJ Jenkins <ajenkins@kyruus.com>
2017-10-29 17:08:47 -0400

    Rewrote result function using knowledge base and action methods

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -146,23 +146,10 @@ class AirCargoProblem(Problem):
         :param action: Action applied
         :return: resulting state after action
         """
-        # Copied from example_have_cake.py
-        # TODO: Re-implement using act()
-        new_state = FluentState([], [])
-        old_state = decode_state(state, self.state_map)
-        for fluent in old_state.pos:
-            if fluent not in action.effect_rem:
-                new_state.pos.append(fluent)
-        for fluent in action.effect_add:
-            if fluent not in new_state.pos:
-                new_state.pos.append(fluent)
-        for fluent in old_state.neg:
-            if fluent not in action.effect_add:
-                new_state.neg.append(fluent)
-        for fluent in action.effect_rem:
-            if fluent not in new_state.neg:
-                new_state.neg.append(fluent)
-        return encode_state(new_state, self.state_map)
+        kb = PropKB()
+        kb.tell(decode_state(state, self.state_map).sentence())
+        action(kb, action.args)
+        return encode_state(FluentState(kb.clauses, []), self.state_map)
 
     def goal_test(self, state: str) -> bool:
         """ Test the state to see if goal is reached
